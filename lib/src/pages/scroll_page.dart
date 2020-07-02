02f7b813f8a258d325553d76446005539a3a3210
mmunozpatinomeli <mercedes.munozpatino@mercadolibre.com>
2020-07-02 20:39:21 -0300

    video 135 ok

--- lib/src/pages/scroll_page.dart
+++ lib/src/pages/scroll_page.dart
@@ -17,14 +17,57 @@ class ScrollPage extends StatelessWidget {
   }
 
   Widget _page1() {
-    return Container(
-      child: Center(child: Text("pagina 1"))
+    return Stack(
+      //respeta como pila el orden del arreglo
+      children: <Widget>[
+        _backGroundColor(),
+        _backgroundImage(),
+        _texts()
+      ],
     );
   }
 
   Widget _page2() {
     return Container(
       child: Center(child: Text("pagina 2"))
+    );
+  }
+
+  Widget _backGroundColor() {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      color: Color.fromRGBO(108, 192, 218, 1.0)
+    );
+  }
+
+  Widget _backgroundImage() {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      child: Image(
+        image: AssetImage('assets/scroll-1.png'),
+        fit: BoxFit.cover
+      ),
+    );
+  }
+
+  Widget _texts() {
+    final textStyle = TextStyle(
+      color: Colors.white,
+      fontSize: 50.0
+    );
+    return SafeArea(
+          child: Column(
+        children: <Widget>[
+          SizedBox(height: 20.0,),
+          Text('11º', style: textStyle),
+          Text('Miercoles', style: textStyle),
+          //crece para los costados y para arriba, nos centró de paso 
+          Expanded(child: Container()),
+          Icon(Icons.keyboard_arrow_down, size: 70.0, color: Colors.white)
+        ],
+      ),
     );
   }
 }
